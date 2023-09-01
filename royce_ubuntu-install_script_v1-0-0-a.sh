7bcc3f8bc9fe23975454b778d5326830a6c0f3c9
Nick Thompson <49480739+NickThompson42@users.noreply.github.com>
2023-09-01 16:58:25 -0500

    cut unneeded code and updated miniconda install

--- royce_ubuntu-install_script_v1-0-0-a.sh
+++ royce_ubuntu-install_script_v1-0-0-a.sh
@@ -1,101 +1,15 @@
 #!/bin/bash
 
-# install instructions
-# to install follow these steps:
-
-# install_h2ogpt() {
-#   wget -O royce_ubuntu-install_script_v1-0-0-a.sh https://github.com/Royce-Geospatial-Consultants/h2ogpt_rg/blob/wor-508_build-h2ogpt_rg-in-codespace/royce_ubuntu-install_script_v1-0-0-a.sh
-#   chmod +x royce_ubuntu-install_script_v1-0-0-a.sh
-#   sudo ./royce_ubuntu-install_script_v1-0-0-a.sh
-# }
-
+# install command
+# curl https://raw.githubusercontent.com/Royce-Geospatial-Consultants/h2ogpt_rg/wor-508_build-h2ogpt_rg-in-codespace/royce_ubuntu-install_script_v1-0-0-a.sh | bash
 
 setup_environment() {
     sudo localectl set-keymap us
     sudo localectl set-x11-keymap us
-
-    # while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
-    #     echo "Waiting for other software managers to finish..."
-    #     sleep 5
-    # done
-
-    # sudo apt update
-    # sudo apt install -y linux-headers-$(uname -r)
-
-    # while sudo fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
-    #     echo "Waiting for other software managers to finish..."
-    #     sleep 5
-    # done
-
-    # echo "Waiting 10 seconds to install NVIDIA drivers"
-    # sleep 10
-
-    # # Install NVIDIA driver
-    # sudo apt install -y nvidia-driver-470 # Or whichever is the latest
 }
 
 # Call the function to execute it
 setup_environment
-
-# Delay replaced with a check to ensure driver is properly installed
-# if dpkg -l | grep -q 'nvidia-driver-470'; then
-#     echo "Successfully installed NVIDIA drivers."
-# else
-#     echo "NVIDIA driver installation failed."
-#     exit 1
-# fi
-
-# sudo systemctl restart gdm3
-
-# load_kernel_modules() {
-#   if ! lsmod | grep -q 'nvidia'; then
-#     sudo modprobe nvidia
-#   fi
-# }
-
-# # Check if nvidia-smi is on the PATH
-# if which nvidia-smi >/dev/null 2>&1; then
-#   echo "nvidia-smi is on the PATH and can be executed."
-
-#   # Try executing nvidia-smi
-#   if ! nvidia-smi; then
-#     echo "nvidia-smi found, but couldn't communicate with NVIDIA driver. Attempting to fix..."
-    
-#     # Step 1: Load kernel modules
-#     echo "Loading kernel modules..."
-#     load_kernel_modules
-
-#     # Re-try executing nvidia-smi
-#     if nvidia-smi; then
-#       echo "Successfully fixed the issue by loading kernel modules."
-#       exit 0
-#     else
-#       echo "Failed to fix the issue. Further investigation is needed."
-#       exit 1
-#     fi
-#   fi
-
-# else
-#   echo "nvidia-smi is not on the PATH. Attempting to locate and add to PATH."
-
-#   # Locate nvidia-smi
-#   NVIDIA_SMI_PATH=$(sudo find / -name "nvidia-smi" 2>/dev/null | head -n 1)
-
-#   if [ -n "$NVIDIA_SMI_PATH" ]; then
-#     # Extract the directory containing nvidia-smi
-#     NVIDIA_SMI_DIR=$(dirname "$NVIDIA_SMI_PATH")
-
-#     # Add the directory to PATH in a new shell session to avoid affecting the current session
-#     echo "export PATH=\$PATH:$NVIDIA_SMI_DIR" >> ~/.bashrc
-
-#     # Notify user to reload .bashrc or open a new shell session
-#     echo "nvidia-smi has been added to the PATH. Please open a new shell or run 'source ~/.bashrc' to update the PATH."
-#   else
-#     echo "nvidia-smi not found. Please check the NVIDIA driver installation."
-#   fi
-# fi
-
-# echo "The NVIDIA-SMI to PATH function check is complete, if you don't see a message about it before this note, then something is wrong."
 
 # Update and install git
 sudo apt-get update -y
@@ -109,7 +23,16 @@ fi
 git --version
 
 # Install Miniconda
-curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh | bash -s -- -b -p $HOME/miniconda3
+install_miniconda() {
+  # Download the Miniconda installer using curl
+  curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  
+  # Make the downloaded script executable
+  chmod +x Miniconda3-latest-Linux-x86_64.sh
+  
+  # Run the Miniconda installer in silent mode
+  echo "yes" | ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+}
 
 # Verify the installation by checking if 'conda' command is available
 source $HOME/miniconda3/etc/profile.d/conda.sh
