34dc05eeb10b12053c7d5d6f12b6bb2fa6be3d12
Nick <nick.thompson3853@gmail.com>
2023-10-11 21:49:33 +0000

    updated copy on copy ssk key script

--- scripts2/copy_ssh_key.sh
+++ scripts2/copy_ssh_key.sh
@@ -7,19 +7,14 @@ copy_ssh_key() {
         # Generate a new SSH key pair
         ssh-keygen -t rsa -b 4096 -f "${key_path}" -N ""
 
-        # Copy the public key to the clipboard
-        # Using xclip for Linux. If on macOS, replace with `pbcopy`
-        cat "${key_path}.pub" | xclip -selection clipboard
-
         # Print messages
         echo "A new key pair was created."
-        echo "Your public key is copied to your clipboard, you may paste it now."
     else
-        # Copy the existing public key to the clipboard
-        cat "${key_path}.pub" | xclip -selection clipboard
-
         # Print messages
         echo "A key pair already existed."
-        echo "Your public key is copied to your clipboard, you may paste it now."
     fi
+
+    # Print the public key to the console
+    echo "Your public key is:"
+    cat "${key_path}.pub"
 }
