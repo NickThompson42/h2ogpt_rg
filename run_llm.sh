17b44be36e4092f4a8ca826e8ae83f45a53b3128
Nick <nick.thompson3853@gmail.com>
2023-10-11 21:33:58 +0000

    added shell to run llm and also doctr.txt file that was missing.

--- run_llm.sh
+++ run_llm.sh
@@ -5,7 +5,7 @@ python generate.py \
     --gradio_offline_level=1 \
     --base_model=h2oai/h2ogpt-gm-oasst1-en-2048-falcon-7b-v3 \
     --score_model=None \
-    --load_4bit=False \
+    --load_4bit=True \
     --prompt_type=human_bot \
     --user_path='user_path' \
     --allow_upload_to_user_data=True
